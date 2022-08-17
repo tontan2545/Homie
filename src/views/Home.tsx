0e3c61a787690136be0c958a86be45e3f1b8e366
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-08-18 02:00:14 +0700

    ✨ add search add to home

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,16 +3,18 @@ import { Text, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "@components/Header";
-import { Container, VStack } from "native-base";
+import { Box, VStack } from "native-base";
+import SearchAdd from "@components/views/Home/SearchAdd";
+import JobPhase from "@components/views/Home/JobPhase";
 
 const Home = () => {
   return (
     <SafeAreaView>
-      <VStack paddingX={3}>
-        <Header />
-        <View>
-          <Text>Home jaa</Text>
-        </View>
+      <Header />
+      <Box height={3} />
+      <VStack paddingX={8} space={3}>
+        <SearchAdd />
+        <JobPhase />
       </VStack>
     </SafeAreaView>
   );
