e4c6c661dff73b673f7a5201110c95ccc6be466e
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-08-17 23:46:01 +0700

    ✨ Home header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,58 @@
+import { HStack, Icon, IconButton } from "native-base";
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  MaterialIcons,
+  Ionicons,
+  AntDesign,
+  FontAwesome,
+} from "@expo/vector-icons";
 
 const Header = () => {
   return (
-    <View>
-      <Text>Header</Text>
-    </View>
+    <HStack justifyContent={"space-between"} alignItems={"center"} paddingY={1}>
+      <IconButton
+        icon={<Icon as={MaterialIcons} name="menu" color="black" size="xl" />}
+        borderRadius="full"
+        _pressed={{
+          bg: "gray.600:alpha.10",
+        }}
+      />
+
+      <HStack>
+        <IconButton
+          icon={
+            <Icon as={AntDesign} name="shoppingcart" color="black" size="xl" />
+          }
+          borderRadius="full"
+          _pressed={{
+            bg: "gray.600:alpha.10",
+          }}
+        />
+        <IconButton
+          icon={
+            <Icon
+              as={Ionicons}
+              name="chatbubble-outline"
+              color="black"
+              size="xl"
+            />
+          }
+          borderRadius="full"
+          _pressed={{
+            bg: "gray.600:alpha.10",
+          }}
+        />
+        <IconButton
+          icon={
+            <Icon as={FontAwesome} name="user-circle" color="black" size="xl" />
+          }
+          borderRadius="full"
+          _pressed={{
+            bg: "gray.600:alpha.10",
+          }}
+        />
+      </HStack>
+    </HStack>
   );
 };
 
