a1ca75284a59cba973640b67d0c4ab5e5ede2776
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-08-18 02:24:17 +0700

    ⚡️ splash screen + preload fonts

--- App.tsx
+++ App.tsx
@@ -1,24 +1,51 @@
 import { NavigationContainer } from "@react-navigation/native";
-import AppLoading from "expo-app-loading";
-import { useFonts } from "expo-font";
-import { NativeBaseProvider } from "native-base";
+import { loadAsync } from "expo-font";
+import { NativeBaseProvider, View } from "native-base";
 import NavigationStack from "./src/routes";
+import * as SplashScreen from "expo-splash-screen";
+import React, { useCallback } from "react";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const [loadedFonts] = useFonts({
-    "Kanit-Bold": require("./assets/fonts/kanit/Kanit-Bold.ttf"),
-    "Kanit-Medium": require("./assets/fonts/kanit/Kanit-Medium.ttf"),
-    "Kanit-Regular": require("./assets/fonts/kanit/Kanit-Regular.ttf"),
-    "Kanit-Thin": require("./assets/fonts/kanit/Kanit-Thin.ttf"),
-  });
+  const [appReady, setAppReady] = React.useState(false);
 
-  if (!loadedFonts) return <AppLoading />;
+  useEffect(() => {
+    const preload = async () => {
+      try {
+        await loadAsync({
+          "Kanit-Bold": require("./assets/fonts/kanit/Kanit-Bold.ttf"),
+          "Kanit-Medium": require("./assets/fonts/kanit/Kanit-Medium.ttf"),
+          "Kanit-Regular": require("./assets/fonts/kanit/Kanit-Regular.ttf"),
+          "Kanit-Thin": require("./assets/fonts/kanit/Kanit-Thin.ttf"),
+        });
+        setAppReady(true);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    preload();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appReady]);
+
+  if (!appReady) {
+    return null;
+  }
 
   return (
     <NativeBaseProvider>
-      <NavigationContainer>
-        <NavigationStack />
-      </NavigationContainer>
+      <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+        <NavigationContainer>
+          <NavigationStack />
+        </NavigationContainer>
+      </View>
     </NativeBaseProvider>
   );
 }
