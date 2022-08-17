9f6c3fbe383e63c70c245ddb888627ecb96e7e47
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-08-17 23:45:35 +0700

    ⚡️ alias path for absolute path reference

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["."],
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+          alias: {
+            "@components": ["./src/components"],
+            "@routes": ["./src/routes"],
+            "@views": ["./src/views"],
+          },
+        },
+      ],
+    ],
   };
 };
