3241d7f1706d7ceeecd5414c4f5602331e6417c5
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-08-17 23:46:30 +0700

    🚧 Home

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,15 +2,18 @@ import { Text, View } from "react-native";
 
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import Header from "@/components/Header";
+import Header from "@components/Header";
+import { Container, VStack } from "native-base";
 
 const Home = () => {
   return (
     <SafeAreaView>
-      <Header />
-      <View>
-        <Text>Home</Text>
-      </View>
+      <VStack paddingX={3}>
+        <Header />
+        <View>
+          <Text>Home jaa</Text>
+        </View>
+      </VStack>
     </SafeAreaView>
   );
 };
