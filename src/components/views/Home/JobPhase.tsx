1a687ae2ded81e5b91a9c924c53ee8b526999593
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-08-18 02:53:36 +0700

    🚧 work in progress job phase

--- src/components/views/Home/JobPhase.tsx
+++ src/components/views/Home/JobPhase.tsx
@@ -1,20 +1,43 @@
-import { Text } from "react-native";
+import { Text } from "@components/common";
 import React from "react";
-import { VStack } from "native-base";
+import { Card, HStack, Icon, VStack } from "native-base";
+import { Entypo } from "@expo/vector-icons";
+import { Bar } from "react-native-progress";
+import Phase from "@customTypes/Phase";
+import PhaseTitle from "./PhaseTitle";
 
-type Props = {};
+type Props = {
+  phase: Phase;
+};
 
-const JobPhase = (props: Props) => {
+const JobPhase = ({ phase }: Props) => {
   return (
-    <VStack>
-      <Text
-        style={{
-          fontSize: 24,
-          fontWeight: "400",
-        }}
+    <VStack space={1}>
+      <PhaseTitle phase={phase} />
+      <Card
+        backgroundColor={"#FCFCFC"}
+        borderColor={"#CFCFCF"}
+        borderStyle="solid"
+        borderWidth={1}
+        borderRadius={10}
       >
-        ✏️ ร่าง
-      </Text>
+        <HStack justifyContent={"space-between"} alignItems="center">
+          <Text style={{ fontSize: 20, fontFamily: "Kanit-Light" }}>
+            งานไม่มีชื่อ
+          </Text>
+          <Icon as={Entypo} name="circle" color="black" />
+        </HStack>
+        {phase === "ONGOING" && (
+          <HStack alignItems={"center"} space={2}>
+            <Bar color="black" progress={0.2} />
+            <Text>20%</Text>
+          </HStack>
+        )}
+
+        <Text style={{ fontSize: 30, fontFamily: "Kanit-Medium" }}>
+          ฿ 19,000
+        </Text>
+      </Card>
     </VStack>
   );
 };
