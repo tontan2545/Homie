9d42eb76d350b7a292388b574102c892be952410
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-08-18 02:00:28 +0700

    ⚡️ load fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,19 @@
 import { NavigationContainer } from "@react-navigation/native";
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
 import { NativeBaseProvider } from "native-base";
 import NavigationStack from "./src/routes";
 
 export default function App() {
+  const [loadedFonts] = useFonts({
+    "Kanit-Bold": require("./assets/fonts/kanit/Kanit-Bold.ttf"),
+    "Kanit-Medium": require("./assets/fonts/kanit/Kanit-Medium.ttf"),
+    "Kanit-Regular": require("./assets/fonts/kanit/Kanit-Regular.ttf"),
+    "Kanit-Thin": require("./assets/fonts/kanit/Kanit-Thin.ttf"),
+  });
+
+  if (!loadedFonts) return <AppLoading />;
+
   return (
     <NativeBaseProvider>
       <NavigationContainer>
