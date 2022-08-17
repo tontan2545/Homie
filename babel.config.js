4987653dc229632d8b9aa9b3cfa4ca00e908d2ae
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-08-18 02:00:55 +0700

    ✨ hook alias

--- babel.config.js
+++ babel.config.js
@@ -12,6 +12,7 @@ module.exports = function (api) {
             "@components": ["./src/components"],
             "@routes": ["./src/routes"],
             "@views": ["./src/views"],
+            "@hooks": ["./src/hooks"],
           },
         },
       ],
