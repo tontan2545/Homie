2913ae8576f3dbaff4be87244ac9b828608a145e
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-08-18 13:30:04 +0700

    ✨ job phase component

--- src/components/views/Home/JobPhase.tsx
+++ src/components/views/Home/JobPhase.tsx
@@ -1,43 +1,26 @@
-import { Text } from "@components/common";
 import React from "react";
-import { Card, HStack, Icon, VStack } from "native-base";
-import { Entypo } from "@expo/vector-icons";
-import { Bar } from "react-native-progress";
+import { Box, FlatList, VStack } from "native-base";
 import Phase from "@customTypes/Phase";
 import PhaseTitle from "./PhaseTitle";
+import JobCard from "./JobCard";
+import { MOCK_JOBS } from "mocks/jobs";
 
 type Props = {
   phase: Phase;
 };
 
 const JobPhase = ({ phase }: Props) => {
+  const phaseJobs = MOCK_JOBS.filter((job) => job.phase === phase);
   return (
     <VStack space={1}>
       <PhaseTitle phase={phase} />
-      <Card
-        backgroundColor={"#FCFCFC"}
-        borderColor={"#CFCFCF"}
-        borderStyle="solid"
-        borderWidth={1}
-        borderRadius={10}
-      >
-        <HStack justifyContent={"space-between"} alignItems="center">
-          <Text style={{ fontSize: 20, fontFamily: "Kanit-Light" }}>
-            งานไม่มีชื่อ
-          </Text>
-          <Icon as={Entypo} name="circle" color="black" />
-        </HStack>
-        {phase === "ONGOING" && (
-          <HStack alignItems={"center"} space={2}>
-            <Bar color="black" progress={0.2} />
-            <Text>20%</Text>
-          </HStack>
-        )}
-
-        <Text style={{ fontSize: 30, fontFamily: "Kanit-Medium" }}>
-          ฿ 19,000
-        </Text>
-      </Card>
+      <VStack space={3}>
+        <FlatList
+          data={phaseJobs}
+          renderItem={(job) => <JobCard {...job.item} />}
+          ItemSeparatorComponent={() => <Box style={{ height: 15 }} />}
+        />
+      </VStack>
     </VStack>
   );
 };
