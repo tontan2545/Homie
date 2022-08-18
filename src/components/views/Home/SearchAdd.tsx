9cb2b6e27d5bb2267c02f6a09f82fbb39527c30d
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-08-18 15:06:16 +0700

    👽️ functional add button

--- src/components/views/Home/SearchAdd.tsx
+++ src/components/views/Home/SearchAdd.tsx
@@ -2,8 +2,14 @@ import React from "react";
 import { HStack, Icon } from "native-base";
 import { IconButton, LinearGradient } from "@components/common";
 import { AntDesign } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "@customTypes/RootStackParamList";
+
+type NewJobScreenProps = NativeStackNavigationProp<RootStackParamList, "Home">;
 
 const SearchAdd = () => {
+  const navigation = useNavigation<NewJobScreenProps>();
   return (
     <HStack justifyContent={"space-between"} alignItems={"center"}>
       <LinearGradient
@@ -15,6 +21,9 @@ const SearchAdd = () => {
           icon={<Icon as={AntDesign} name={"plus"} color="white" size="lg" />}
           size="xs"
           borderRadius={"xl"}
+          onPress={() => {
+            navigation.navigate("NewJob");
+          }}
         />
       </LinearGradient>
       <IconButton
