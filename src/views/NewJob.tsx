b8d1bfd32bf586a51f8ab5f9afcc505293071fef
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-08-20 16:31:35 +0700

    🚧 integrate stepper to new job

--- src/views/NewJob.tsx
+++ src/views/NewJob.tsx
@@ -1,14 +1,31 @@
-import { View, Text } from "react-native";
+import { Box } from "native-base";
+import NewJobHeader from "@components/Header/NewJobHeader";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import NewJobHeader from "@components/Header/NewJobHeader";
+import SelectJobType from "@components/views/NewJob/SelectJobType";
+import SetJobName from "@components/views/NewJob/SetJobName";
 import Stepper from "@components/views/NewJob/Stepper";
+import { useNewJobStore } from "@hooks/useNewJobStore";
+
+const getStepComponent = (step: number) => {
+  switch (step) {
+    case 0:
+      return <SetJobName />;
+    case 1:
+      return <SelectJobType />;
+    default:
+      return <></>;
+  }
+};
 
 const NewJob = () => {
+  const { step } = useNewJobStore();
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
       <NewJobHeader />
       <Stepper />
+      <Box height={10} />
+      {getStepComponent(step)}
     </SafeAreaView>
   );
 };
