173d7492ce182d620ef5116c07b98a8ca95c01e2
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-08-20 16:31:22 +0700

    🎨 job type map

---
+++ src/constants/jobTypeList.ts
@@ -0,0 +1,10 @@
+import { JobType } from "@customTypes/JobTypes";
+
+export const jobTypeList: Record<string, JobType> = {
+  หลังคา: "ROOF",
+  ฝ้า: "CEILING",
+  สี: "PAINT",
+  พื้น: "FLOOR",
+  พนัง: "WALL",
+  ระบบประปา: "WATER",
+};
